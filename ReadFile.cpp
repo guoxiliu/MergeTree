5397916077e2b7735453ab6abd2a51fe2fbd8a90
Guoxi <liuguoxi@chd.edu.cn>
2019-11-25 10:59:38 -0500

    Add basic structure for merge tree
    Also modify the file reader

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -7,34 +7,35 @@ using namespace std;
 int main ( int argc, char *argv[] )
 {
   //parse command line arguments
-  if(argc < 2)
-  {
-    cerr << "Usage: " << argv[0]
-              << " Filename(.vtu)" << endl;
+  if(argc < 2){
+    cerr << "Usage: " << argv[0] << " Filename(.vtu)" << endl;
     return EXIT_FAILURE;
   }
 
-  vtkSmartPointer<vtkXMLDataReader> reader;
 
   string filename = argv[1];
   string extension = filename.substr(filename.length() - 3);
 
-  if(extension == "vtu"){
-    //read unstructed grid
-    reader = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
-    reader->SetFileName(filename.c_str());
-    reader->Update();
-  } else if(extension == "vti"){
-    // read image data
-    reader = vtkSmartPointer<vtkXMLImageDataReader>::New();
-    reader->SetFileName(filename.c_str());
-    reader->Update();
+  if(extension != "vtu"){
+    cout << "The file extension should be .vtu!\n";
+    return -1;
   }
+
+  vtkSmartPointer<vtkXMLUnstructuredGridReader> reader = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
+  reader->SetFileName(filename.c_str());
+  reader->Update();
 
   vtkIdType cellNum = reader->GetNumberOfCells();
   vtkIdType pointNum = reader->GetNumberOfPoints();
   cout << "There are " << cellNum << " cells in the triangulation.\n";
   cout << "There are " << pointNum << " points in the triangulation.\n";
+
+  // TODO: Create the merge tree here.
+
+
+
+  // TODO: Test the queries here.
+
 
   return EXIT_SUCCESS;
 }
---
+++ mergetree.h
@@ -0,0 +1,53 @@
+#include <vector>
+#include <vtkSmartPointer.h>
+#include <vtkUnstructuredGrid.h>
+
+using namespace std;
+
+// TODO: Super Arc class.
+class SuperArc{
+
+};
+
+
+/**
+ * Merge Tree Class.
+ * The merge tree is created by combining join tree and split tree, which 
+ * captures the evolution of the superlevel or sublevel sets.
+ */ 
+class MergeTree{
+public:
+  MergeTree(vtkSmartPointer<vtkUnstructuredGrid> p){
+    usgrid = p;
+    group = vector<int>(p->GetNumberOfPoints(), -1);
+  }
+
+  // TODO: Implement the topological queries.
+
+  // TODO: Maxima query
+
+  // TODO: Component maximum query
+
+
+protected:
+  vector<int> group;    // Used for Union-Find algorithm
+  vtkSmartPointer<vtkUnstructuredGrid> usgrid;    // Unstructed grid
+  vector<SuperArc> arcs;    // Save the point set?
+
+private:
+  // Find the group id of a given vertex id.
+  int findGroup(int i){
+    if(group[i] == -1)
+      return i;
+    return findGroup(group[i]);
+  }
+
+  // Do union of two groups.
+  void unionGroup(int x, int y){
+    int xset = findGroup(x);
+    int yset = findGroup(y);
+    if(xset != yset){
+      group[xset] = yset;
+    }
+  }
+};
